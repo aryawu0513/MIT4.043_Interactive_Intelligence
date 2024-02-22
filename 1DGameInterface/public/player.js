a7f6899a77d4902eaf19768e17d25b82f3cd8c7e
aryawu0513 <aryawu0513@gmail.com>
2024-02-22 14:44:22 -0500

    trying server

---
+++ 1DGameInterface/public/player.js
@@ -0,0 +1,47 @@
+// This holds some player information, like color and position.
+// It also has some player methods for managing how a player moves.
+
+class Player {
+  constructor(_color, _position, _displaySize) {
+    this.playerColor = _color;
+    this.position = _position;
+    this.displaySize = _displaySize;
+    this.level = 0; // Initialize the level to 0
+    this.isDashing = false; // Initialize dashing flag
+    this.voidTimer = 0;
+  }
+
+  // Move player based on keyboard input
+  move(_direction) {
+    // increments or decrements player position
+    this.position = this.position + _direction;
+
+    // if player hits the edge of display, loop around
+    if (this.position < 0) {
+      //==-1
+      this.position = this.displaySize - 1;
+    } else if (this.position >= this.displaySize) {
+      this.position -= this.displaySize;
+      this.level++;
+    }
+  }
+  moveWhileDashing(_direction) {
+    if (this.isDashing) {
+      this.move(_direction);
+      setTimeout(() => this.moveWhileDashing(_direction), 50); // Call moveWhileDashing again after 1 second
+    }
+  }
+  startDash(_direction) {
+    this.isDashing = true;
+    this.moveWhileDashing(1); // Start moving immediately
+    // setTimeout(() => {
+    //   if (this.isDashing) {
+    //     this.stopDash();
+    //   }
+    // }, 3000);
+  }
+
+  stopDash() {
+    this.isDashing = false;
+  }
+}
---
+++ 1DGameInterface/public/sketch.js
@@ -0,0 +1,72 @@
+/* /////////////////////////////////////
+
+  4.043 / 4.044 Design Studio: Interaction Intelligence
+  February 9, 2024
+  Marcelo Coelho
+
+*/ /////////////////////////////////////
+
+var socket;
+let displaySize = 30; // how many pixels are visible in the game
+let pixelSize = 20; // how big each 'pixel' looks on screen
+
+let playerOne; // Adding 2 players to the game
+let playerTwo;
+let target; // and one target for players to catch.
+
+let display; // Aggregates our final visual output before showing it on the screen
+
+let controller; // This is where the state machine and game logic lives
+
+// let collisionAnimation; // Where we store and manage the collision animation
+let fallAnimation;
+
+let score; // Where we keep track of score and winner
+
+let ground = [];
+
+let sound;
+
+function preload() {
+  // Load the sound file during preload
+  sound = loadSound("robotsound.mp3");
+  //console.log(sound);
+}
+
+function setup() {
+  socket = io.connect("http://localhost:3000");
+  frameRate(60);
+  //createCanvas(displaySize * pixelSize, pixelSize); // dynamically sets canvas size
+  createCanvas(displaySize * pixelSize, 5 * pixelSize);
+  for (let i = 0; i < displaySize; i++) {
+    // Randomly assign ground or void
+    ground[i] = random() < 0.8 ? "GROUND" : "VOID";
+  }
+  ground[0] = ground[3] = "GROUND";
+
+  display = new Display(displaySize, pixelSize, ground); //Initializing the display
+  display.setGround(ground);
+  //console.log(ground);
+  playerOne = new Player(color(255, 0, 0), parseInt(3), displaySize); // Initializing players
+  playerTwo = new Player(color(0, 0, 255), parseInt(0), displaySize);
+
+  display.setGradientColors(3, 0, color(255, 0, 0), color(0, 0, 255));
+  tangleAnimation = new closeAnimation(); // Initializing animation
+  breakAnimation = new farAnimation();
+  fallAnimationOne = new FallOne(playerOne, playerTwo, display);
+  fallAnimationTwo = new FallTwo(playerOne, playerTwo, display);
+  controller = new Controller(); // Initializing controller
+
+  //score = { max: 3, winner: color(0, 0, 0) }; // score stores max number of points, and color
+}
+
+function draw() {
+  // start with a blank screen
+  background(51, 51, 51);
+
+  // Runs state machine at determined framerate
+  controller.update();
+
+  // After we've updated our states, we show the current one
+  display.show();
+}
